b07bf55f35e20d3f87e04c8db101e3c3fc82c68f
Keerthivasan <Keerthik1407@gmail.com>
2021-03-18 23:24:09 -0400

    Menu list to the Mini jarvis has been designed

--- calc.sh
+++ calc.sh
@@ -1,11 +1,19 @@
 #! /usr/bin/env bash
 
 # When the program is first loaded, display a greeting to the user.
+echo "*****************************************************************************************************************"
 echo "Welcome to the Impresive Interactive Online-calculator, Mini Jarvis:) Lets start rolling with Digits. Here you go!"
+echo "******************************************************************************************************************"
 # Then, display a menu that outlines the possible operations:
   # Add
   # Subtract
   # Exit
+
+echo "Mini-Jarvis Calculator Menu" 
+echo "1) Addition (+)     2) Subtraction (-)"
+echo "3) Division (/)     4) Multiplciation (*)"
+
+echo "Dear Legendary User, select the available option that you want to try. Best of Luck"
 # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
