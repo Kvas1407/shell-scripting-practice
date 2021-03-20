ab1a633db379ada48d17da618c4462465f38fc30
Keerthivasan <Keerthik1407@gmail.com>
2021-03-20 19:19:40 -0400

    Arithmetic code has been implemented in to the program for Trail Run

--- calc.sh
+++ calc.sh
@@ -8,11 +8,15 @@ echo "**************************************************************************
   # Add
   # Subtract
   # Exit
+while true
+do
 
-echo "Mini-Jarvis Calculator Menu" 
-echo "1) Addition (+)     2) Subtraction (-)"
-echo "3) Division (/)     4) Multiplciation (*)"
-echo "5) Exit"
+echo "Mini-Jarvis Calculator Menu
+1) Addition (+)     
+2) Subtraction (-)
+3) Division (/)     
+4) Multiplciation (*)
+5) Exit"
 
 # Then, capture the user selection.
 echo "Dear Legendary User:) Please type the available operation that you want to try. Best of Luck"
@@ -20,5 +24,47 @@ read operation
 
 echo "You have selected Operation: $operation"
 # If the selection matches a supported operation, execute the operation.
+echo "Please enter the Valid 2 Numbers of your choice: "
+read num1
+read num2
+
+until [ $num1 != NULL ] && [ $num2 != NULL ]; do
+
+	echo "Sorry:( Invalid number entries. Please enter the Valid 2 number again: "
+read num1
+read num2
+done
+
+case $operation in 
+	"Addition" | "addition" | "1")
+		let "sum=$num1 + $num2"
+		echo "The Result of your selected Operation - Addition is: $sum"
+		;;
+
+	"Subtraction" | "subtraction" | "2")
+		let "sub=$num1 - $num2"
+		echo "The Result of your selected Operation - Subtraction is: $sub"
+		;;
+
+	"Multiplication" | "multiplication" | "4")
+		let "multiply=$num1 * $num2"
+		echo "The Result of your selected Operation - Multiplication is: $multiply"
+		;;
+
+	"Division" | "division" | "3")
+		let "divide=$num1 / $num2"
+		echo "The result of your selected Operation - Division is: $divide"
+		;;
+
+	"Exit" | "exit" | "5")
+		echo "Thank you for Using Mini-jarvis, Be Kind! Stay Safe"
+		exit
+		;;
+	*)
+		echo "Sorry, Mini Jarvis was not programmed for the selected Operation. For More information:  Soon, Mega-jarvis are suppose to be launched,
+		in which you can find more options to play around:)"
+		;;
+esac
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
+done
